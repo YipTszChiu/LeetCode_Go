b1025fd37399789a777cd5bd4bfcf36785f39d4e
YipTszChiu <515061462@qq.com>
2022-03-24 09:04:06 +0800

    2022.03.24

---
+++ BitOperation/Add.go
@@ -0,0 +1,15 @@
+package BitOperation
+
+//剑指 Offer 65. 不用加减乘除做加法
+//https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/
+
+//当前位相加等于异或 a ^= b，进制位置等于相与
+func add(a int, b int) int {
+	for b > 0 {
+		c := (a & b) << 1 //c为进位
+		a ^= b            //a为当前位，与b异或后存到a
+		b = c             //将进制位赋给b，如果b不为0需要继续将进制位加上
+	}
+
+	return a
+}
